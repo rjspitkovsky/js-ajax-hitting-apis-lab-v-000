bf863b81a98c1c9b79318d1a7a0aa21a1f7201c4
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-05-12 21:25:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,20 @@ function getRepositories() {
   xml.open("GET", url)
   xml.send()
 }
+
+function displayRepositories() {
+  const repos = JSON.parse(this.responseText)
+  const repoList = "<ul>" + repos.map(repo => {
+    const dataUserName = 'data-username="' + repo.owner.login + '"'
+    const dataRepoName = 'data-repository="' + repo.name + '"'
+    return(`
+      <li> 
+        <h2>${repo.name}</h2>
+        <a href="${repo.html_url}">${repo.html_url}</a><br>
+          <a href="#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
+          <a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">Get Branches</a></li>
+        </li>`
+      )
+  }).join("") + "</ul>"
+  document.getElementById("repositores").innerHTML = repoList 
+}
