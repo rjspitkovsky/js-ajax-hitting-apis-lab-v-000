1995008265a41c79dd897cd2c13a9d2928f3e32a
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-05-12 21:17:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 const baseURL = "https://api.github.com"
 
+function getRepositories() {
+  const name = document.getElementById("username").value
+  const url = baseURL + "/users" + name + "/repos"
+  const xml = new XMLHttpRequest()
+  xml.addEventListener("load", displayRepositories)
+  xml.open("GET", url)
+  xml.send()
+}
