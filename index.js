19cdeb88f320d243b5b514204f7479dc4ea81fe2
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-05-12 21:40:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const baseURL = "https://api.github.com"
 
 function getRepositories() {
   const name = document.getElementById("username").value
-  const url = baseURL + "/users" + name + "/repos"
+  const url = baseURL + "/users/" + name + "/repos"
   const xml = new XMLHttpRequest()
   xml.addEventListener("load", displayRepositories)
   xml.open("GET", url)
