72a1560bd97dcd2c54ca4a4ba3d69a2288f64290
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-05-12 21:35:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -34,4 +34,10 @@ function getCommits(el) {
   xml.send()
 }
 
-function d
+function displayCommits() {
+  const commits = JSON.parse(this.responseText)
+  const commitsList = "<ul>" + commits.map(commit => {
+    `<li><h3>` + commit.commit.author.name + ' (' + commit.author.login + ')</h3>' + commit.commit.message + '</li>'.join('')
+  }) + "</ul>"
+
+}
